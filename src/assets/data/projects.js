3cfa52a8f53bad91a6b74fcbe83c175b1c31ca1d
Vincent Journot <journot.vincent@gmail.com>
2022-11-05 00:00:14 +0100

    update projects with my informations

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,50 +1,104 @@
 import { v4 as uuidv4 } from 'uuid';
-import p1 from '../images/p1.jpg';
-import p2 from '../images/p2.jpg';
-import p3 from '../images/p3.jpg';
-import p4 from '../images/p4.png';
-import p5 from '../images/p5.jpg';
+import bookshelf_amj from '../images/bookshelf_amj.JPG';
+import workbench from '../images/workbench.png';
+import onewink from '../images/onewink.png'
+import toolchest from '../images/toolchest.JPG'
+import tyb from '../images/tyb.png'
+import woodbox from '../images/woodbox.JPG'
+import pmo from '../images/pmo.jpg'
+import finance from '../images/finance.jpg'
+import lion from '../images/lion.jpg'
+import pixel from '../images/pixel.png'
+import pulse from '../images/pulse.jpg'
 
 const projects = [
     {
         id: uuidv4(),
-        category: "Woodworking",
-        name: 'U Tracker',
+        category: "Web dev",
+        name: 'Onewink website',
         desc:
-        'An application to track your all data from one place. I developed the website and the mobile app',
-        img: p1,
+        'Retrofit of an association website. Addition of a timeline feature editable by the owner. Build with Django and React frameworks.',
+        img: onewink,
+    },
+    {
+        id: uuidv4(),
+        category: "Web dev",
+        name: 'Customization app',
+        desc:
+        'Application to create a custom design UV printed on freestyle scooter wheels. Build with Django and React frameworks.',
+        img: lion,
     },
     {
         id: uuidv4(),
         category: "Woodworking",
-        name: 'Green CTG',
+        name: 'Floating bookshelf',
         desc:
-        'An app to help people to get an overview of how they can make the city beautiful.',
-        img: p2,
+        'A shelf made from oak wood inherited from my father. 2 levels shelf with mouldings made with handtools.',
+        img: bookshelf_amj,
     },
     {
         id: uuidv4(),
-        category: "Web Dev",
-        name: 'Coin Tracker',
+        category: "Web dev",
+        name: 'Project manager app',
         desc:
-        'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
-        img: p3,
+        'Central system to automate project management tasks. Extract, transform and load data from an API automated with Powershell.',
+        img: pmo,
     },
     {
         id: uuidv4(),
-        category: "Web Dev",
-        name: "Cavin's Portfolio",
+        category: "Web dev",
+        name: 'Accounts manager',
         desc:
-        'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
-        img: p4,
+        'Financial app to centralize transactions of an entire family with several bank accounts. Build with Django python framework.',
+        img: finance,
+    },
+    {
+        id: uuidv4(),
+        category: "Woodworking",
+        name: "Woodboxes",
+        desc:
+        "Woodboxes made with dovetail joints by hand.",
+        img: woodbox,
+    },
+    {
+        id: uuidv4(),
+        category: "Woodworking",
+        name: 'Dutch toolchest',
+        desc:
+        'Traditional toolchest to store my precious handtools with a secret compartment.',
+        img: toolchest,
+    },
+    {
+        id: uuidv4(),
+        category: "Woodworking",
+        name: 'My workbench',
+        desc:
+        'My first traditional workbench with different clamping system. Assembly with traditional joints.',
+        img: workbench,
     },
     {
         id: uuidv4(),
         category: "Tech",
-        name: 'Tracking Soft',
+        name: 'Tune your bike',
         desc:
-        'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
-        img: p5,
+        'Arduino system to measure, record and analyze impact of suspension\'s settings on my montain bike.',
+        img: tyb,
+    },
+    {
+        id: uuidv4(),
+        category: "Tech",
+        name: 'Heart Rate Variability',
+        desc:
+        'Analyze of my pulse signal captured with smartphone to determined my HRV. Data processed with Excel.',
+        img: pulse,
+    },
+    {
+        id: uuidv4(),
+        category: "Tech",
+        name: 'Discrete to continuous',
+        desc:
+        'Study of the impact of computer graphics in the field of medical imaging.',
+        img: pixel,
     },
 ];
 
