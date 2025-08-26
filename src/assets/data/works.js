de3b2db701c7c72cc5181d680e21f76badf3c8d1
Vincent Journot <journot.vincent@gmail.com>
2025-08-26 16:08:36 +0200

    update work experiences

--- src/assets/data/works.js
+++ src/assets/data/works.js
@@ -1,24 +1,24 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const works = [
-    {
-        id: uuidv4(),
-        name: '2014-Now',
-        items: ['Project Manager'],
-        appendix: 'for Heraeus Materials SA'
-    },
-    {
-        id: uuidv4(),
-        name: '2013-2014',
-        items: ['Manufacturing Engineer'],
-        appendix: 'for Ethicon Inc.'
-    },
-    {
-        id: uuidv4(),
-        name: '2010-2013',
-        items: ['Junior Process Engineer'],
-        appendix: 'for Greatbatch Medical'
-    },
-]
+  {
+    id: uuidv4(),
+    name: "2014-2024",
+    items: ["Project Manager"],
+    appendix: "for Heraeus Materials SA",
+  },
+  {
+    id: uuidv4(),
+    name: "2013-2014",
+    items: ["Manufacturing Engineer"],
+    appendix: "for Ethicon Inc.",
+  },
+  {
+    id: uuidv4(),
+    name: "2010-2013",
+    items: ["Junior Process Engineer"],
+    appendix: "for Greatbatch Medical",
+  },
+];
 
 export default works;
