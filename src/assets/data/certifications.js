15918eb2bf79adc756c3b7d60d9e2f7b4d89c61a
Vincent Journot <journot.vincent@gmail.com>
2025-11-19 15:19:12 +0100

    add excel certification

--- src/assets/data/certifications.js
+++ src/assets/data/certifications.js
@@ -1,18 +1,24 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const certifications = [
-    {
-        id: uuidv4(),
-        name: '2023',
-        items: ['The Ultimate IT Beginners Course'],
-        appendix: 'certificat issued by TechWorld With Nana'
-    },
-    {
-        id: uuidv4(),
-        name: '2022',
-        items: ['Entry-Level Python Programmer'],
-        appendix: '[PCEP-30-02] certificat issued by Python Institute'
-    },
-]
+  {
+    id: uuidv4(),
+    name: "2025",
+    items: ["Microsoft Excel: Beginner to Advanced - AI"],
+    appendix: "certificat issued by Tutorialspoint",
+  },
+  {
+    id: uuidv4(),
+    name: "2023",
+    items: ["The Ultimate IT Beginners Course"],
+    appendix: "certificat issued by TechWorld With Nana",
+  },
+  {
+    id: uuidv4(),
+    name: "2022",
+    items: ["Entry-Level Python Programmer"],
+    appendix: "[PCEP-30-02] certificat issued by Python Institute",
+  },
+];
 
 export default certifications;
